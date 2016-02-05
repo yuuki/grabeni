1ed235cb325d5b1a7c73355bd4f591e2126fe7f0
y_uuki <yuki.tsubo@gmail.com>
2016-02-05 22:31:46 +0900

    Add TestPrintENIs

--- format/format_test.go
+++ format/format_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/stretchr/testify/assert"
 
@@ -21,5 +22,30 @@ func TestPrintENI(t *testing.T) {
 	PrintENI(w, eni)
 
 	expected = "ID\tNAME\tSTATUS\tPRIVATE DNS NAMEPRIVATE IP\tAZ\tDEVICE INDEX\tINSTANCE ID\tINSTANCE NAME\nID\tNAME\tSTATUS\tPRIVATE DNS NAMEPRIVATE IP\tAZ\tDEVICE INDEX\tINSTANCE ID\tINSTANCE NAME\n\t\t\t\t\t\t\t\t-1\t\t\t\t\n"
+	assert.Equal(t, expected, string(w.Bytes()))
+}
+
+func TestPrintENIs(t *testing.T) {
+	w := new(bytes.Buffer)
+	PrintENIs(w, nil)
+
+	expected := "ID\tNAME\tSTATUS\tPRIVATE DNS NAMEPRIVATE IP\tAZ\tDEVICE INDEX\tINSTANCE ID\tINSTANCE NAME\n"
+	assert.Equal(t, expected, string(w.Bytes()))
+
+	w = new(bytes.Buffer)
+	enis := make([]*model.ENI, 2)
+	enis[0] = model.NewENI(&ec2.NetworkInterface{})
+
+	i := model.NewInstance(&ec2.Instance{
+		InstanceId: aws.String("i-1000000"),
+		Tags: []*ec2.Tag{&ec2.Tag{
+			Key: aws.String("Name"),
+			Value: aws.String("grabeni001"),
+		}},
+	})
+	enis[0].SetInstance(i)
+	PrintENIs(w, enis)
+
+	expected = "ID\tNAME\tSTATUS\tPRIVATE DNS NAMEPRIVATE IP\tAZ\tDEVICE INDEX\tINSTANCE ID\tINSTANCE NAME\n\t\t\t\t\t\t\t\t-1\t\ti-1000000\tgrabeni001\n"
 	assert.Equal(t, expected, string(w.Bytes()))
 }
