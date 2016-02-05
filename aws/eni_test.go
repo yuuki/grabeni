b0cb16a43fba85d6b24a64b1b1962c340749dad8
y_uuki <yuki.tsubo@gmail.com>
2016-02-06 01:53:31 +0900

    Add TestDescribeInstancesByID

--- aws/eni_test.go
+++ aws/eni_test.go
@@ -43,3 +43,44 @@ func TestDescribeInstanceByID(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "i-00000001", i.InstanceID())
 }
+
+func TestDescribeInstancesByID(t *testing.T) {
+	mockEC2 := new(EC2API)
+	c := newClient(mockEC2)
+
+	mockEC2.On("DescribeInstances", &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String("i-00000001")},
+	}).Return(&ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{
+			&ec2.Reservation{
+				Instances: []*ec2.Instance{&ec2.Instance{
+					InstanceId: aws.String("i-00000001"),
+				}},
+			},
+		},
+	}, nil)
+
+	instances, err := c.DescribeInstancesByIDs([]string{"i-00000001"})
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(instances))
+	assert.Equal(t, "i-00000001", instances[0].InstanceID())
+
+
+	mockEC2 = new(EC2API)
+	c = newClient(mockEC2)
+
+	mockEC2.On("DescribeInstances", &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{aws.String("i-00000000")},
+	}).Return(&ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{
+			&ec2.Reservation{Instances: nil},
+		},
+	}, nil)
+
+	instances, err = c.DescribeInstancesByIDs([]string{"i-00000000"})
+
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(instances))
+}
+
