0cc4a02ef8793db143e7de4c02096d552f805b39
y_uuki <yuki.tsubo@gmail.com>
2016-03-02 08:49:11 +0900

    Enable to build in docker container

--- script/release.sh
+++ script/release.sh
@@ -7,6 +7,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+ROOT=$(dirname $0)/..
+
 # gofmt
 gofmt -s -w .
 git add ./*.go
@@ -19,13 +21,7 @@ git commit -m "Bump version $new_version"
 git push origin master
 
 # build release files
-goxc -tasks='xc archive' -bc 'linux,!arm windows darwin' -d .
-cp -p $(PWD)/snapshot/linux_amd64/grabeni $(PWD)/snapshot/grabeni_linux_amd64
-cp -p $(PWD)/snapshot/linux_386/grabeni $(PWD)/snapshot/grabeni_linux_386
-cp -p $(PWD)/snapshot/darwin_amd64/grabeni $(PWD)/snapshot/grabeni_darwin_amd64
-cp -p $(PWD)/snapshot/darwin_386/grabeni $(PWD)/snapshot/grabeni_darwin_386
-cp -p $(PWD)/snapshot/windows_amd64/grabeni.exe $(PWD)/snapshot/grabeni_windows_amd64.exe
-cp -p $(PWD)/snapshot/windows_386/grabeni.exe $(PWD)/snapshot/grabeni_windows_386.exe
+"$ROOT"/script/build_in_container.sh "$ROOT"/script/build_release.sh
 
 # make github release draft
 ghr --username yuuki1 --replace --draft "v$new_version" snapshot/
