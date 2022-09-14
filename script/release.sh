b2c8d3fa8a71a5f65644e058ca25cc059b60c6b9
yuuki <yuki.tsubo@gmail.com>
2022-09-15 08:59:12 +0900

    Remove unnessesary release files

--- script/release.sh
+++ script/release.sh
@@ -14,9 +14,3 @@ new_version=$(gobump "$1" -w -v cmd/grabeni | jq -r '.[]')
 git add ./*.go
 git commit -m "Bump version $new_version"
 git push origin master
-
-# build release files
-"$ROOT"/script/build_in_container.sh "$ROOT"/script/build_release.sh
-
-# make github release draft
-ghr --username yuuki --replace --draft "v$new_version" snapshot/
